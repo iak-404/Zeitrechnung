c1176206dbe90bef9307bf37e9737597e8193126
Kai <d4k189@outlook.de>
2024-11-23 17:23:34 +0100

    first commit

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    static class Year
+    {
+        public static int secondPerMinute = 60;
+        public static int secondPerHour = secondPerMinute * 60;
+        public static int secondPerDay = secondPerHour * 24;
+        public static int secondPerWeek = secondPerDay * 7;
+        public static int secondPerMonth = secondPerDay * 30;
+        public static int secondPerYear = secondPerMonth * 12;
+        public static int minutePerHour = 60;
+        public static int minutePerDay = minutePerHour * 24;
+        public static int minutePerWeek = minutePerDay * 7;
+        public static int minutePerMonth = minutePerDay * 30;
+        public static int minutePerYear = minutePerMonth * 12;
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿//Werte
+using System.ComponentModel.Design;
+using System.Threading.Channels;
+using test;
+
+//Eingabe
+string Eingabe;
+
+//Formel
+int SecPM = Year.secondPerMinute;
+int SecPH = Year.secondPerHour;
+int SecPD = Year.secondPerDay;
+int SecPW = Year.secondPerWeek;
+int SecPMo = Year.secondPerMonth;
+int SecPY = Year.secondPerYear;
+int MinPH = Year.minutePerHour;
+int MinPD = Year.minutePerDay;
+int MinPW = Year.minutePerWeek;
+int MinPM = Year.minutePerMonth;
+int MinPY = Year.minutePerYear;
+
+do
+{
+    Console.WriteLine("Sekunden und Minuten ausrechnen, was möchtest du Wissen?");
+    Console.WriteLine("Für alle Kürzel, gib einfach \"help\" und für das Verlasse \"exit\" ein.");
+
+    Console.Write("Deine Eingabe: ");
+
+    Eingabe = Console.ReadLine();
+
+    if (Eingabe == "help")
+    {
+        Console.WriteLine("Sekunden pro Minute = SecPM \n" +
+                          "Sekunden pro Stunde = SecPH \n" +
+                          "Sekunden pro Tag    = SecPD \n" +
+                          "Sekunden pro Woche  = SecPW \n" +
+                          "Sekunden pro Monat  = SecPMo \n" +
+                          "Sekunden pro Jahr   = SecPY \n" +
+                          "Minuten pro Stunde  = MinPH \n" +
+                          "Minuten pro Tag     = MinPD \n" +
+                          "Minuten pro Woche   = MinPW \n" +
+                          "Minuten pro Monat   = MinPM \n" +
+                          "Minuten pro Jahr    = MinPY");
+    }
+    else if (Eingabe == "SecPM")
+    {
+        Console.WriteLine(SecPM);
+    }
+    else if (Eingabe == "SecPH")
+    {
+        Console.WriteLine(SecPH);
+    }
+    else if (Eingabe == "SecPD")
+    {
+        Console.WriteLine(SecPD);
+    }
+    else if (Eingabe == "SecPW")
+    {
+        Console.WriteLine(SecPW);
+    }
+    else if (Eingabe == "SecPMo")
+    {
+        Console.WriteLine(SecPMo);
+    }
+    else if (Eingabe == "SecPY")
+    {
+        Console.WriteLine(SecPY);
+    }
+    else if (Eingabe == "MinPH")
+    {
+        Console.WriteLine(MinPH);
+    }
+    else if (Eingabe == "MinPD")
+    {
+        Console.WriteLine(MinPD);
+    }
+    else if (Eingabe == "MinPW")
+    {
+        Console.WriteLine(MinPW);
+    }
+    else if (Eingabe == "MinPM")
+    {
+        Console.WriteLine(MinPM);
+    }
+    else if (Eingabe == "MinPY")
+    {
+        Console.WriteLine(MinPY);
+    }
+    else
+    {
+        Console.WriteLine("Falsche Angabe!");
+    }
+
+
+}
+
+while (Eingabe != "exit");
+
+
+
